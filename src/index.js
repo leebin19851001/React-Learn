9b5a0fac18fb434a4dcb121e12966c89ffe4b39b
libin_ss <libin_ss@kyee.com.cn>
2019-12-07 11:14:44 +0800

    useState

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById('root'));
-
+ReactDOM.render(<A/>, document.getElementById('root'));
+function A() {
+    const [n, setN] = useState(0);
+    return (
+        <>
+           <button onClick={()=> {
+               setN(n - 1)
+           }}>-</button>
+           <span style={{width:40,color:'red',display:'inline-block',textAlign:'center'}}>{n}</span>
+           <button onClick={()=> {
+               setN(n + 1)
+           }}>+</button> 
+        </>
+    )
+}
